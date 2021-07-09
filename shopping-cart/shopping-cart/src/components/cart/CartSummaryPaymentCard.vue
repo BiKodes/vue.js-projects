9f6e462d898f5ec741794b5069b840476fd8ef11
BikoPython <stevennyandoya@gmail.com>
2021-07-09 20:40:22 +0300

    vue-3-todo-app

---
+++ shopping-cart/shopping-cart/src/components/cart/CartSummaryPaymentCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="cart-item-card">
+        <h3>Cart Total: Ksh.{{ cart_total.toFixed(2) }}</h3>
+
+        <button class="view-product-button">Pay by Credit Card</button> | 
+        <button class="view-product-button">Pay by M-Pesa</button>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        cart_total(){
+            return this.$store.getters.cartTotal
+        }
+       
+    }
+}
+</script>
+
+<style lang="scss">
+    
+</style>
---
+++ shopping-cart/shopping-cart/src/data/items.js
@@ -0,0 +1,39 @@
+const items = [
+    {
+        id: 1,
+        name: "Title Punching Bag",
+        price: 15000.00,
+        category: 'Sports',
+        description: "Punching bags are the oldest form of boxing and combat sport training dating back more than a century. And still today, punching bags provide the most advanced and beneficial training available. TITLE Boxing features every style and type of punching bags from traditional heavy bags, full-length Thai heavy bags, uppercut heavy bags, water filled heavy bags, wall mount heavy bags, freestanding heavy bags, heavy bag stands and hangers, speed bags, speed bag platforms, double end bags, and every accessory available to complete your punching bag demands. Brands featuring TITLE Boxing, Fighting, Pro Mex, Rival, Adidas, Century and more."
+    },
+    {
+        id: 2,
+        name: "Mouth Guard",
+        price: 3000.00,
+        category: 'Sports',
+        description: "Punching bags are the oldest form of boxing and combat sport training dating back more than a century. And still today, punching bags provide the most advanced and beneficial training available. TITLE Boxing features every style and type of punching bags from traditional heavy bags, full-length Thai heavy bags, uppercut heavy bags, water filled heavy bags, wall mount heavy bags, freestanding heavy bags, heavy bag stands and hangers, speed bags, speed bag platforms, double end bags, and every accessory available to complete your punching bag demands. Brands featuring TITLE Boxing, Fighting, Pro Mex, Rival, Adidas, Century and more."
+    },
+    {
+        id: 3,
+        name: "Muay Thai Shorts",
+        price: 2500.00,
+        category: 'Sports',
+        description: "Punching bags are the oldest form of boxing and combat sport training dating back more than a century. And still today, punching bags provide the most advanced and beneficial training available. TITLE Boxing features every style and type of punching bags from traditional heavy bags, full-length Thai heavy bags, uppercut heavy bags, water filled heavy bags, wall mount heavy bags, freestanding heavy bags, heavy bag stands and hangers, speed bags, speed bag platforms, double end bags, and every accessory available to complete your punching bag demands. Brands featuring TITLE Boxing, Fighting, Pro Mex, Rival, Adidas, Century and more."
+    },
+    {
+        id: 4,
+        name: "Boxing Wraps",
+        price: 8000.00,
+        category: 'Sports',
+        description: "Punching bags are the oldest form of boxing and combat sport training dating back more than a century. And still today, punching bags provide the most advanced and beneficial training available. TITLE Boxing features every style and type of punching bags from traditional heavy bags, full-length Thai heavy bags, uppercut heavy bags, water filled heavy bags, wall mount heavy bags, freestanding heavy bags, heavy bag stands and hangers, speed bags, speed bag platforms, double end bags, and every accessory available to complete your punching bag demands. Brands featuring TITLE Boxing, Fighting, Pro Mex, Rival, Adidas, Century and more."
+    },
+    {
+        id: 5,
+        name: "Gloves",
+        price: 15000.00,
+        category: 'Sports',
+        description: "Punching bags are the oldest form of boxing and combat sport training dating back more than a century. And still today, punching bags provide the most advanced and beneficial training available. TITLE Boxing features every style and type of punching bags from traditional heavy bags, full-length Thai heavy bags, uppercut heavy bags, water filled heavy bags, wall mount heavy bags, freestanding heavy bags, heavy bag stands and hangers, speed bags, speed bag platforms, double end bags, and every accessory available to complete your punching bag demands. Brands featuring TITLE Boxing, Fighting, Pro Mex, Rival, Adidas, Century and more."
+    },
+]
+
+export default items
